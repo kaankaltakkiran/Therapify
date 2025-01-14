03a7496cd532fc5d97835d3993af49d6e3477ce2
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-01-14 22:46:41 +0300

    try

--- Frontend/src/pages/IndexPage.vue
+++ Frontend/src/pages/IndexPage.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page>
     <!-- Hero Section -->
+    <h2>saasddddddddddddddd</h2>
+
     <section class="hero-section">
       <div class="container q-pa-md">
         <div class="row items-center">
