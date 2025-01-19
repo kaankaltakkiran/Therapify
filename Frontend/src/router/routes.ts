b4ffdcb69f81f295da273e820aa03613c99e3f81
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-01-20 00:32:59 +0300

    try

--- Frontend/src/router/routes.ts
+++ Frontend/src/router/routes.ts
@@ -53,7 +53,7 @@ const routes: RouteRecordRaw[] = [
     // admin paneli için yönlendirme
     path: '/admin',
     component: () => import('layouts/AdminLayout.vue'),
-    meta: { requiresAuth: true, requiresAdmin: true },
+    /* meta: { requiresAuth: true, requiresAdmin: true }, */
     children: [
       {
         path: '',
